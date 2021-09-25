43cd08823f5f737ff9684c2496d38000255dec62
Gurov Evgeniy <egurov@ozon.ru>
2021-09-25 23:33:55 +0300

    1. two sum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/evro23x/myleetcode
+
+go 1.17
---
+++ solution/1/two_num.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+		seenNums := make(map[int]int)
+		for index, thisNum := range nums {
+			if seenIndex, ok := seenNums[target - thisNum]; ok {
+				return []int{seenIndex, index}
+			}
+			seenNums[thisNum] = index
+		}
+	return []int{}
+}
+
+func main() {
+	nums := []int{3,2,3}
+	target := 9
+	twoSum(nums, target)
+	fmt.Println(1)
+}
+
+//func twoSum(nums []int, target int) []int {
+//	seenNums := make(map[int]int)
+//	for index, thisNum := range nums {
+//		if seenIndex, ok := seenNums[target - thisNum]; ok {
+//			return []int{seenIndex, index}
+//		}
+//		seenNums[thisNum] = index
+//	}
+//	return []int{0, 0} // Should not happen
+//}
+
+
+//func twoSum(nums []int, target int) []int {
+//	lookup := make(map[int]int)
+//	for i, v := range nums {
+//		j, ok := lookup[-v]
+//		lookup[v - target] = i
+//		if ok {
+//			return []int{j, i}
+//		}
+//	}
+//	return []int{}
+//}
+
+//func twoSum(nums []int, target int) []int {
+//	numsMap := make(map[int]int, len(nums))
+//	for i, v := range nums {
+//		numsMap[v] = i
+//	}
+//	for i, v := range nums {
+//		if j, ok := numsMap[target - v]; ok && j != i {
+//			return []int{i, j}
+//		}
+//	}
+//	return []int{}
+//}
